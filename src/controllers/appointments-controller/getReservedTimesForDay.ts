79cb2b26a9b6fa114520f6f450d302cc487bd82b
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-01-05 23:12:25 +0200

    fix date format

--- src/controllers/appointments-controller/getReservedTimesForDay.ts
+++ src/controllers/appointments-controller/getReservedTimesForDay.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { format, parseISO } from "date-fns";
+import { format, parse } from "date-fns";
 import ReservedTime from "../../models/ReservedTime.js";
 
 const timesInterval = [
@@ -15,7 +15,7 @@ const getReservedTimesForDay = async (req: Request, res: Response) => {
   const { id, date: dateString } = req.params;
   // const date = new Date(dateString);
   // const formattedDate = date.toISOString();
-  const date = parseISO(dateString);
+  const date = parse(dateString, "yyyy-MM-dd", new Date());
   const formattedDate = format(date, "yyyy-MM-dd");
 
   const data = await ReservedTime.find({

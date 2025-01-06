8aed92a229809568d34c5f6f70f67ef4255e5b50
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-01-06 16:05:42 +0200

    fix

--- src/controllers/appointments-controller/index.ts
+++ src/controllers/appointments-controller/index.ts
@@ -1,4 +1,4 @@
-export { default as addAppointment } from "./addAppointmentNotLoggedInUser.js";
 export { default as getReservedTimesForDay } from "./getReservedTimesForDay.js";
+export { default as addAppointment } from "./addAppointment.js";
 export { default as addReservedTimeForDay } from "./addReservedTimeForDay.js";
 export { default as appointmentIsComplete } from "./appointmentIsComplete.js";

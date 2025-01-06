9a253a19aabc8335d240ea0681e44f2edbb0b746
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-01-06 21:21:58 +0200

    fix date format

--- src/controllers/appointments-controller/addAppointment.ts
+++ src/controllers/appointments-controller/addAppointment.ts
@@ -6,7 +6,9 @@ import { HttpError } from "../../helpers/index.js";
 
 const addAppointment = async (req: Request, res: Response) => {
   const { psychologistId } = req.body;
+  console.log("req.body.date", req.body.date);
   const date = format(req.body.date, "yyyy-MM-dd");
+  console.log(date);
   const psychologist = await Psychologist.findById(psychologistId);
   if (!psychologist) {
     throw HttpError(404, `psychologist with id ${psychologistId} not found`);
